0e4e5d4b4b71a0e128129ec4ac9e9286e0eb610e
Luis Ángel Méndez Gort <gort.andres000@gmail.com>
2017-02-15 11:14:44 -0500

    Parsing CSV documents with ; as separator

--- PruebasParser/PruebaCSV.cs
+++ PruebasParser/PruebaCSV.cs
@@ -7,7 +7,8 @@ namespace PruebasParser
 	[TestFixture()]
 	public class PruebaCSV
 	{
-		string book1CSV = @"Book1.csv", sep = ";";
+		string book1CSV = @"Book1.csv", sep = ";",
+		garbarinoCSV = @"garbarino.csv", decena = "decena__01985.csv";
 
 		[Test()]
 		public void TestParseCSV()
@@ -16,7 +17,23 @@ namespace PruebasParser
 			string[][] p = { new string[] { "col1", "col2", "col3" } };
 			Documento d = Parser.LoadParseCSV(book1CSV, sep, p, ref r);
 			Assert.IsTrue(d != null && r == null && d.Tabla.GetLength(0) == 1
-			              && d.Tabla.GetLength(1) == 3);
+						  && d.Tabla.GetLength(1) == 3);
+
+			Documento dn;
+			p = new string[][]{new string[] { "X" },
+				new string[] { "X", "Importe" },
+				new string[] { "Importe", "Moneda", "Factura" } };
+			dn = Parser.LoadParseCSV(garbarinoCSV, sep, p, ref r);
+			Assert.IsTrue(d != null && r == null && dn.Tabla.GetLength(0) == 15 &&
+						  dn.Tabla.GetLength(1) == 3);
+
+			Documento dec;
+			p = new string[][]{
+				new string[]{"operador","fecha","out","pax","importe","iva","MONEDA","NRO_CONF"}};
+			dec = Parser.LoadParseCSV(decena, sep, p, ref r);
+			Assert.IsTrue(d != null && r == null &&
+						  dec.Tabla.GetLength(1) == p[0].Length &&
+						  dec.Tabla.GetLength(0) == 26);
 		}
 	}
 }
